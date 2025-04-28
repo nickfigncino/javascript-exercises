bce87278637f97f897fef024c574c55b7345f71c
Nick Figliolia <nick.figliolia@ncino.com>
2025-04-28 14:04:07 -0400

    09 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	// Clean the string by converting to lowercase and removing all non-alphanumeric characters
+	const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+	// Reverse the cleaned string
+	const reversed = cleaned.split("").reverse().join("");
+
+	// Check if the cleaned string equals its reverse
+	return cleaned === reversed;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+	const num = Number(input);
+	if (num < 0) return "OOPS";
+	if (num === 0) return 0;
+	if (num === 1) return 1;
 
+	let a = 0;
+	let b = 1;
+	for (let i = 2; i <= num; i++) {
+		const temp = a + b;
+		a = b;
+		b = temp;
+	}
+	return b;
 };
 
 // Do not edit below this line
